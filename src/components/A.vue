2e32a5922c4f3ebe5cf8f6af713afec7984493da
fuyongde <fuyongde@foxmail.com>
2020-05-03 19:30:36 +0800

    :sparkles: 新增 Vuex 的基础用法

--- src/components/A.vue
+++ src/components/A.vue
@@ -2,5 +2,24 @@
   <div>
     这是组件A
     <router-link to="/router_demo">返回</router-link>
+    <br />
+    <div>count : {{ count }}</div>
+    <button @click="countIncrease">点我自增</button>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'A',
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    countIncrease() {
+      this.$store.commit('countIncrease');
+    }
+  }
+}
+</script>
