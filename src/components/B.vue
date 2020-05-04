1f1aa7d120f0a72bda3612b703a9b270cc6b9215
fuyongde <fuyongde@foxmail.com>
2020-05-04 09:28:08 +0800

    :sparkles: 在 B 组件中演示 vuex 的多组件共享数据

--- src/components/B.vue
+++ src/components/B.vue
@@ -2,5 +2,17 @@
   <div>
     这是组件B
     <router-link to="/router_demo">返回</router-link>
+    <div>count : {{ count }}</div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'A',
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  }
+}
+</script>
